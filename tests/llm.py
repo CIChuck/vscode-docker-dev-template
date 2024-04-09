baf311b7ebf2469d8a169a08727b8f31570edfeb
cichuck <crussell@collectiveintelligence.com>
2024-04-09 17:45:56 -0400

    system p

--- tests/llm.py
+++ tests/llm.py
@@ -5,26 +5,28 @@ from openai import OpenAI
 
 class OAI: 
 
-    def __init__(self):
-        
+    def __init__(self, systemPrompt="You are a helpful assistant."):
+
+        super.__init__()
         self.client = OpenAI()
+        self.systemPrompt = {"role": "system", "content": systemPrompt}
+
         load_dotenv()
 
     def get_chatresponse(self, prompt, temperature = .5, max_tokens = 50,model = "gpt-4-0125-preview"):
-
-        client = OpenAI()
 
         response = self.client.chat.completions.create(
             model = model,
             max_tokens = max_tokens,
             temperature = temperature,
             messages=[
-                {"role": "system", "content": "You are a helpful assistant."},
+                self.systemPrompt,
                 {"role": "user", "content": prompt},
                 ]
             )
 
         return response.choices[0].message.content
+    
     
     def get_JSON_response(self, prompt, temperature = .5, max_tokens = 50,model = "gpt-4-0125-preview"):
         
@@ -34,7 +36,7 @@ class OAI:
             max_tokens = max_tokens,
             temperature = temperature,
             messages=[
-                {"role": "system", "content": "You are a helpful assistant."},
+                self.systemPrompt,
                 {"role": "user", "content": prompt},
                 ]
             )
